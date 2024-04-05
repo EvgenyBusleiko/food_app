c4cc25c93d9615e2ffdbc45c0b0b745c75a42e6e
EvgenyBusleiko <busleiko@mail.ru>
2024-04-05 14:26:04 +0300

    Production version

--- final_exam/settings/production.py
+++ final_exam/settings/production.py
@@ -7,16 +7,16 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
-    'Ilippy.pythonanywhere.com',
+    'shooter.pythonanywhere.com',
 ]
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'Ilippy$jornapal',
-        'USER': 'Ilippy',
+        'NAME': 'shooter$food_app',
+        'USER': 'shooter',
         'PASSWORD': os.getenv('MYSQL_PASSWORD'),
-        'HOST': 'Ilippy.mysql.pythonanywhere-services.com',
+        'HOST': 'shooter.mysql.pythonanywhere-services.com',
         'OPTIONS': {
             'init_command': "SET NAMES 'utf8mb4'; SET sql_mode='STRICT_TRANS_TABLES'",
             'charset': 'utf8mb4'
